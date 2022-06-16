6108d3a9b675e06e080d748a3d87b88fa2a7b6da
ThienBienBlue <TN.QG.Nguyen+GitHub@gmail.com>
2022-06-16 12:54:20 -0700

    Evil mode c-u now works like vim.

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -1,7 +1,8 @@
+(setq evil-want-C-u-scroll t
+      evil-insert-state-cursor 'box)
+
 (use-package evil
   :config
-  (setq evil-want-C-u-scroll t
-        evil-insert-state-cursor 'box)
   (require 'evil)
   (evil-mode 1))
 
