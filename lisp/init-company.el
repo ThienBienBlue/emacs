ff9701f161af07b6e2c63bc76f9cf517f3b50258
ThienBienBlue <TN.QG.Nguyen+GitHub@gmail.com>
2022-06-23 00:39:37 -0700

    Changed ordering of `init-company.el`

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -2,14 +2,14 @@
   :requires yasnippet
   :hook global-company-mode
   :config
-  (setq company-minimum-prefix-length 1
-        company-idle-delay 0.0
-        company-selection-wrap-around t)
   (define-key company-active-map (kbd "C-e") 'company-abort)
   (define-key company-active-map (kbd "C-y") 'company-complete-selection)
   (define-key company-active-map (kbd "<return>") 'company-complete-common)
   (define-key company-active-map (kbd "RET") 'company-complete-common)
+  (define-key company-active-map (kbd "<tab>") 'yas-next-field-or-maybe-expand)
   (define-key company-active-map (kbd "TAB") 'yas-next-field-or-maybe-expand)
-  (define-key company-active-map (kbd "<tab>") 'yas-next-field-or-maybe-expand))
+  (setq company-minimum-prefix-length 1
+        company-idle-delay 0.0
+        company-selection-wrap-around t))
 
 (provide 'init-company)
