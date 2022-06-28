37b39ca9d29ce495e4331b9d5a769daf00a7a80a
ThienBienBlue <TN.QG.Nguyen+GitHub@gmail.com>
2022-06-27 21:29:11 -0700

    `fido-vertical-mode` falls back onto `fido-mode`.

--- lisp/init-fuzzy-completion.el
+++ lisp/init-fuzzy-completion.el
@@ -1,5 +1,8 @@
 (setq completion-styles '(initials partial-completion flex))
 (setq completion-cycle-threshold 10)
-(fido-mode)
+
+(condition-case err
+    (fido-vertical-mode)
+  (error (fido-mode)))
 
 (provide 'init-fuzzy-completion)
