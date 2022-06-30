b016991ff1aba0f29ca642a1a145e207c627dd6a
ThienBienBlue <TN.QG.Nguyen+GitHub@gmail.com>
2022-06-30 12:00:40 -0700

    Default theme mode-line has no box.

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,4 +1,8 @@
-(use-package gruvbox-theme)
-;  :config (load-theme 'gruvbox-dark-hard t))
+(set-face-attribute 'mode-line nil :box nil)
+(set-face-attribute 'mode-line-inactive nil :box nil)
+
+(use-package gruvbox-theme
+  :config
+  (load-theme 'gruvbox-dark-hard t))
 
 (provide 'init-themes)
