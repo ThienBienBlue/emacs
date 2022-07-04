3cddd803c999f3bb1791872e3f9bea21a4da9a52
ThienBienBlue <TN.QG.Nguyen+GitHub@gmail.com>
2022-07-04 01:43:55 -0700

    More work with company backends.

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -2,8 +2,15 @@
   :requires yasnippet
   :hook global-company-mode
   :config
+  (defun setup/company-abort-and-evil-normal ()
+    (interactive)
+    (company-abort)
+    (evil-normal-state))
   (define-key company-active-map (kbd "C-e") 'company-abort)
+  (define-key company-active-map (kbd "C-d") 'company-abort)
   (define-key company-active-map (kbd "C-y") 'company-complete-selection)
+  (define-key company-active-map (kbd "<escape>") 'setup/company-abort-and-evil-normal)
+  (define-key company-active-map (kbd "ESC") 'setup/company-abort-and-evil-normal)
   (define-key company-active-map (kbd "<return>") 'company-complete-common)
   (define-key company-active-map (kbd "RET") 'company-complete-common)
   (define-key company-active-map (kbd "<tab>") 'yas-next-field-or-maybe-expand)
@@ -11,8 +18,12 @@
   (setq company-minimum-prefix-length 1
         company-idle-delay 0.0
         company-selection-wrap-around t
+        company-dabbrev-ignore-case nil
         company-frontends '(company-pseudo-tooltip-frontend
-                            company-preview-frontend
-                            company-echo-metadata-frontend)))
+                            company-echo-metadata-frontend)
+        company-backends '((company-capf
+                            company-dabbrev-code
+                            company-yasnippet))
+        company-transformers '(delete-consecutive-dups)))
 
 (provide 'init-company)
