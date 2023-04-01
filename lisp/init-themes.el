e679d0d513272bf70705a63932f980c72377713d
ThienBienBlue <TN.QG.Nguyen+GitHub@gmail.com>
2023-04-01 11:41:13 -0700

    Back to dark mode.

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,4 +1,9 @@
 (set-face-attribute 'mode-line nil :box nil)
 (set-face-attribute 'mode-line-inactive nil :box nil)
 
+(set-foreground-color "#e4e4ef")
+(set-background-color "#181818")
+(set-cursor-color "#ffdd33")
+(set-face-attribute 'line-number-current-line nil :foreground "#ffdd33")
+
 (provide 'init-themes)
