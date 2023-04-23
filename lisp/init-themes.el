9c995b34ac8ee549e8bad516b112445a4200c97a
ThienBienBlue <TN.QG.Nguyen+GitHub@gmail.com>
2023-04-23 15:44:35 -0700

    Muted out foreground color for fonts.

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,7 +1,7 @@
 (set-face-attribute 'mode-line nil :box nil)
 (set-face-attribute 'mode-line-inactive nil :box nil)
 
-(set-foreground-color "#e4e4ef")
+(set-foreground-color "#e0e0e0")
 (set-background-color "#181818")
 (set-cursor-color "#ffdd33")
 (set-face-attribute 'line-number-current-line nil :foreground "#ffdd33")
