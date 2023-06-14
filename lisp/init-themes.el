4aaa45339e9d69af7dee5a35a395e4c5cfe775cc
ThienBienBlue <TN.QG.Nguyen+GitHub@gmail.com>
2023-06-13 23:09:41 -0700

    Added more acceptable default fonts.
    These are Fira Mono, JetBrains Mono, Ubuntu Mono.

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -8,8 +8,11 @@
 (load-theme 'tango-dark)
 
 (setq font-name (seq-find (lambda (name) (find-font (font-spec :name name)))
-                          '("Fira Code"
-                            "Source Code Pro")))
+                          '("Fira Mono"
+                            "Fira Code"
+                            "JetBrains Mono"
+                            "Source Code Pro"
+                            "Ubuntu Mono")))
 (if font-name
     (set-face-attribute 'default nil :height 110 :font font-name)
   (set-face-attribute 'default nil :height 110))
