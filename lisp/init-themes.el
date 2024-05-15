c10e0976870ce89e38dc1b7af26b47eabb69d7b9
ThienBienBlue <TN.QG.Nguyen+GitHub@gmail.com>
2024-05-15 07:57:06 -0700

    Try out whitespace mode and mono-color regions.

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,10 +1,17 @@
-;(set-foreground-color "#d7d7d7")
-;(set-background-color "#282828")
-;(set-cursor-color "#ffdd33")
-;(set-face-attribute 'line-number-current-line nil :foreground "#ffdd33")
 (load-theme 'tango-dark)
 (set-face-attribute 'mode-line nil :box nil)
 (set-face-attribute 'mode-line-inactive nil :box nil)
+(set-face-attribute 'region nil :foreground (face-attribute 'default :foreground))
+
+(defface unobtrusive-whitespace
+  '((t :foreground "#1e2426"))
+  "Barely visible unless highlighting.")
+(setq whitespace-style '(face tabs spaces tab-mark space-mark)
+      whitespace-tab 'unobtrusive-whitespace
+      whitespace-space 'unobtrusive-whitespace
+      whitespace-hspace 'unobtrusive-whitespace)
+
+(global-whitespace-mode +1)
 
 (setq-default line-spacing 0.1)
 (setq fonts-found (seq-filter (lambda (name) (find-font (font-spec :name name)))
